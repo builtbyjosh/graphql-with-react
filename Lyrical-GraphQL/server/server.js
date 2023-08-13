9a31f76a2e4ff2d1c3eb13d1af1de2875ef22954
builtbyjosh <joshuacollins912@gmail.com>
2023-08-13 13:00:44 -0500

    connect mongodb

--- Lyrical-GraphQL/server/server.js
+++ Lyrical-GraphQL/server/server.js
@@ -1,38 +1,43 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
+require("dotenv").config();
 
 const app = express();
 
+const mongodbUser = process.env.MONGODB_USER;
+const mongodbPassword = process.env.MONGODB_PASSWORD;
+
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+
+const MONGO_URI = `mongodb+srv://${mongodbUser}:${mongodbPassword}@cluster0.pxo1vf2.mongodb.net/lyrical?retryWrites=true&w=majority`;
 if (!MONGO_URI) {
-  throw new Error('You must provide a Mongo Atlas URI');
+  throw new Error("You must provide a Mongo Atlas URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('open', () => console.log('Connected to Mongo Atlas instance.'))
-  .on('error', (error) =>
-    console.log('Error connecting to Mongo Atlas:', error)
+  .once("open", () => console.log("Connected to Mongo Atlas instance."))
+  .on("error", (error) =>
+    console.log("Error connecting to Mongo Atlas:", error)
   );
 
 app.use(bodyParser.json());
 app.use(
-  '/graphql',
+  "/graphql",
   expressGraphQL({
     schema,
-    graphiql: true
+    graphiql: true,
   })
 );
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
